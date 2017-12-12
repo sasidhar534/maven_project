472c15ec02f4a87e883a63a714e7fca33fb1d4e8
murali <murali.nani4u@gmail.com>
2017-12-13 01:14:45 +0530

    initiating maven project

---
+++ mymvn/src/main/java/com/sasi/test/App.java
@@ -0,0 +1,13 @@
+package com.sasi.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
